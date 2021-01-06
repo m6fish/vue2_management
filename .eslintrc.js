b35e3235358367d72e09ccd299e09deed9c201f7
bulb <bulb.kh.gy@gmail.com>
2021-01-06 17:37:04 +0800

    初始設定

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,20 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+    env: {
+        browser: true,
+        es2021: true
     },
-    "extends": [
-        "plugin:vue/essential",
-        "standard"
+    extends: [
+        'plugin:vue/essential',
+        'standard'
     ],
-    "parserOptions": {
-        "ecmaVersion": 12,
-        "sourceType": "module"
+    parserOptions: {
+        ecmaVersion: 12,
+        sourceType: 'module'
     },
-    "plugins": [
-        "vue"
+    plugins: [
+        'vue'
     ],
-    "rules": {
+    rules: {
+        indent: [2, 4, { SwitchCase: 1 }]
     }
-};
+}
